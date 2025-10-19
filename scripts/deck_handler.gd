bdebc9364562991a5cd1e304bdb03703b7f479fb
Endermandels <elijahdelavar@gmail.com>
2025-10-19 08:07:31 -0700

    Remove scroll up input when hand is already up

--- scripts/deck_handler.gd
+++ scripts/deck_handler.gd
@@ -5,12 +5,13 @@ class_name DeckHandler
 
 @export_group("External Nodes")
 @export var deck: Deck
+@export var hand: Hand
 @export var deck_button: TextureButton
 @export var deck_sprite: CardHandler
 @export var hand_handler: HandHandler
 
 func _ready() -> void:
-    deck_button.pressed.connect(_add_card_to_hand)
+    deck_button.pressed.connect(draw_card)
 
 const FOOD_CARD = preload("res://scenes/food_card.tscn")
 const ANIMAL_CARD = preload("res://scenes/animal_card.tscn")
@@ -32,7 +33,7 @@ func spawn_card(card: Card) -> CardHandler:
 
     return sc
 
-func _add_card_to_hand() -> void:
+func draw_card() -> void:
     var card = deck.draw_card()
 
     if card == null:
@@ -44,6 +45,7 @@ func _add_card_to_hand() -> void:
     else:
         push_warning("Drew Unknown Card")
     
+    hand.add_child(card)
     hand_handler.add_card_to_hand(spawn_card(card))
 
     if deck.is_empty():
