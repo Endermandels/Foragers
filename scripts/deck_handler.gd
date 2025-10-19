0f158724fab0cc6ede926294dbc14a1d1460d761
Endermandels <elijahdelavar@gmail.com>
2025-10-19 08:51:55 -0700

    Add logic for both players to draw 3 cards at start

--- scripts/deck_handler.gd
+++ scripts/deck_handler.gd
@@ -1,17 +1,22 @@
 extends Node2D
 class_name DeckHandler
 
-## This is the deck handler for Player 1 ONLY
+@export_group("Settings")
+@export var is_player_1: bool = false
 
 @export_group("External Nodes")
+@export var game_logic: GameLogic
 @export var deck: Deck
 @export var hand: Hand
-@export var deck_button: TextureButton
+@export var deck_button: TextureButton ## Leave blank for P2
 @export var deck_sprite: CardHandler
-@export var hand_handler: HandHandler
+@export var hand_handler: HandHandler ## Leave blank for P2
 
 func _ready() -> void:
-    deck_button.pressed.connect(draw_card)
+    if deck_button:
+        deck_button.pressed.connect(draw_card)
+        disable_click()
+    game_logic.draw_cards.connect(draw_cards)
 
 const FOOD_CARD = preload("res://scenes/food_card.tscn")
 const ANIMAL_CARD = preload("res://scenes/animal_card.tscn")
@@ -27,27 +32,44 @@ func spawn_card(card: Card) -> CardHandler:
         push_warning("Unknown card type for spawn")
         return sc
     
-    sc.add_child(card)
     sc.match_card_stats(card)
     sc.global_position = deck_sprite.global_position
 
     return sc
+
+func draw_cards(n_cards: int, player_1: bool) -> void:
+    if player_1 == is_player_1:
+        for i in range(n_cards):
+            draw_card()
 
 func draw_card() -> void:
     var card = deck.draw_card()
 
     if card == null:
         print("Empty Deck")
+        disable_click()
+        return
     elif card is FoodCard:
         print("Drew Food Card")
     elif card is AnimalCard:
         print("Drew Animal Card")
     else:
         push_warning("Drew Unknown Card")
+        return
     
     hand.add_child(card)
-    hand_handler.add_card_to_hand(spawn_card(card))
+    if hand_handler:
+        hand_handler.add_card_to_hand(spawn_card(card))
 
     if deck.is_empty():
+        disable_click()
+
+func enable_click() -> void:
+    if deck_button:
+        deck_button.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
+        deck_button.disabled = false
+
+func disable_click() -> void:
+    if deck_button:
         deck_button.mouse_default_cursor_shape = Control.CURSOR_ARROW
         deck_button.disabled = true
--- scripts/game_logic.gd
+++ scripts/game_logic.gd
@@ -1,5 +1,53 @@
 extends Node
 class_name GameLogic
 
+enum GameState {
+    DRAW,
+    BUY,
+    ATTACK
+}
+
 @export_group("Settings")
 @export var is_player_1_turn: bool = true
+@export var n_beginning_cards_p1: int = 3
+@export var n_beginning_cards_p2: int = 3
+
+var turn: int = 0
+var state: GameState = GameState.DRAW
+
+signal draw_cards(n: int, player_1: bool) ## Draw n cards
+signal show_trade_row()
+
+func _ready() -> void:
+    get_tree().create_timer(0.2).timeout.connect(draw_phase)
+
+func draw_phase() -> void:
+    print("Draw state entered")
+    state = GameState.DRAW
+
+    if turn == 0 and is_player_1_turn:
+        draw_cards.emit(n_beginning_cards_p1, true)
+        draw_cards.emit(n_beginning_cards_p2, false)
+    else:
+        draw_cards.emit(1, is_player_1_turn)
+
+func buy_phase() -> void:
+    print("Buy state entered")
+    state = GameState.BUY
+    show_trade_row.emit()
+
+func attack_phase() -> void:
+    print("Attack state entered")
+    state = GameState.ATTACK
+    
+    
+
+func progress_phase() -> void:
+    if state == GameState.ATTACK:
+        turn += 1
+        is_player_1_turn = not is_player_1_turn
+        draw_phase()
+    elif state == GameState.DRAW:
+        buy_phase()
+    elif state == GameState.BUY:
+        attack_phase()
